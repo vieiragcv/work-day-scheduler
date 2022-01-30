1a952d1c03162c68222619c61b6ebe9d9f02ab3c
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-30 02:53:07 -0500

    current time on header and additional visual upgrades

--- assets/moment.js
+++ assets/moment.js
@@ -1,11 +1,21 @@
+var liveTime = moment().hours();
+
+
+var editTask = function(value) {
+  console.log('liveTime is: ' + liveTime);
+  console.log('calendarTime is: ' + value);
+  if (liveTime > value) {
+    console.log('Oops, time machine not found');
+    
+  }
+}
+
+/* checks if the hour value on the live clock is higher than hour interval  */
+
+var clickEl = document.querySelector("#clickTest");
+
+console.log(clickEl);
+
 var liveClock = window.setInterval(function () {
   $('#currentDay').html(moment().format('ddd MM/DD/YYYY H:mm:ss'))
 }, 1000);  
-
-var a = new Date('01/12/2016');
-console.log(a);
-
-
-var b = moment('01/12/2016', 'DD/MM/YYYY', true).format();
-console.log(b);
-
