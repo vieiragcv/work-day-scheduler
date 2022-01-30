495b4e435e9fa05eb1a712c43255a86c759c4944
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-30 00:38:23 -0500

    front-end draft created

--- assets/moment.js
+++ assets/moment.js
@@ -1,10 +1,11 @@
 var liveClock = window.setInterval(function () {
-  $('#clock').html(moment().format('ddd MM/DD/y H:mm:ss'))
+  $('#currentDay').html(moment().format('ddd MM/DD/YYYY H:mm:ss'))
 }, 1000);  
 
 var a = new Date('01/12/2016');
 console.log(a);
-console.log();
+
 
 var b = moment('01/12/2016', 'DD/MM/YYYY', true).format();
 console.log(b);
+
