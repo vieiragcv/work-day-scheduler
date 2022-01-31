c1e1e9e28b0ef57ef71d334b0f87ad28ff5d0b4c
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-30 20:25:25 -0500

    modal adjusted. event listeners added

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,56 @@
 /* Event Listeners  */
 /* JQUERY */
 var taskSlots = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
-var taskIds = [];
-var clickedEl;
-var taskID;
-
-console.log(taskSlots);
-console.log(document.getElementById('8am'));
-
-function findMatchingDiv(value) {
-  for(i=0; i < taskSlots.length; i++) {           // runs array of task hours
-    var x = taskSlots[i];
-    console.log(x);
-    
-    if( x === value){                           // if IDs match
-
-    }
-  }
-}
-
 
 $(document).ready(function(){
 for(i=0; i < taskSlots.length; i++) {
+
   var x = "#" + taskSlots[i];
-  $(x).click(function(event){
-  alert('you have clicked');
+  $(x).click(function(){
+
+    console.log(this);
+
+    $(this).children().remove();
+
+    console.log(this);
+    var createEl = document.createElement("input");
+    createEl.className = '';
+    createEl.type = 'text';
+    createEl.value = 'hello hello hello';
+    this.append(createEl);
+    console.log(createEl);
+
+    //alert('you have clicked');
   });
 }
 });
+
+
+
+/* MODAL */
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+console.log(span);
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+  modal.style.display = "none";
+  }
+}
