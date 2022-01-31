e93b297e731c3955c72a85877fd36beb1126462e
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-30 21:14:50 -0500

    color coding tasks based on current hour

--- assets/moment.js
+++ assets/moment.js
@@ -1,10 +1,13 @@
 /* Logs Current Hour */
 
 var currentHour = moment().hours();
+console.log (currentHour);
 
 //Live Clock on Header 
 
 var liveClock = window.setInterval(function () {
   $('#currentDay').html(moment().format('ddd MM/DD/YYYY H:mm:ss'))
 }, 1000);  
+
+
 
--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,52 @@
 /* Event Listeners  */
 /* JQUERY */
 var taskSlots = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+var taskSlotsiD = ['#8am', '#9am', '#10am', '#11am', '#12pm', '#1pm', '#2pm', '#3pm', '#4pm', '#5pm', '#6pm'];
+var taskSlotsNum = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var currentHour = moment().hours();
 
 $(document).ready(function(){
 for(i=0; i < taskSlots.length; i++) {
 
   var x = "#" + taskSlots[i];
-  $(x).click(function(){
+  $(x).dblclick(function(){
 
-    console.log(this);
+  
 
     $(this).children().remove();
 
-    console.log(this);
+   
     var createEl = document.createElement("input");
     createEl.className = '';
     createEl.type = 'text';
     createEl.value = 'hello hello hello';
     this.append(createEl);
-    console.log(createEl);
+    
 
     //alert('you have clicked');
   });
 }
 });
+
+for(i = 0; i < taskSlotsNum.length; i++){
+  
+  if (currentHour > taskSlotsNum[i]) {
+    $(taskSlotsiD[i]).addClass('past');
+    console.log('gray' + taskSlotsNum[i])
+  }
+  else if (currentHour < taskSlotsNum[i]) {
+    $(taskSlotsiD[i]).addClass('future');
+    console.log('green' + taskSlotsNum[i]);
+  }
+  else {
+    $(taskSlotsiD[i]).addClass('present');
+    console.log('red' + taskSlotsNum[i]);
+  } 
+}
+
+
+
+
 
 
 
