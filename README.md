3a8795635b6ad1bf3f98a38920ae002e72403297
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-30 23:17:07 -0500

    additional fixes, update to readMe and organization of next steps to complete challenge

--- README.md
+++ README.md
@@ -8,28 +8,39 @@ WHEN I open the planner
 THEN the current day is displayed at the top of the calendar 
 
 (COMPLETE)
-
+---------------------------------------------------
 WHEN I scroll down
 THEN I am presented with time blocks for standard business hours
 
 (COMPLETE)
-
+---------------------------------------------------
 WHEN I view the time blocks for that day
 THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 (COMPLETE)
-
+---------------------------------------------------
 WHEN I click into a time block
 THEN I can enter an event
 
 (PENDING)
 
+1 - print on screen the label edits for each task as button is clicked
+
+---------------------------------------------------
 WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 
 (PENDING)
 
+1 - funciton to be called as button is clicked to log entry to local file
+  1.1. -> create array that stores task data and set to local storage
+
+
+---------------------------------------------------
 WHEN I refresh the page
 THEN the saved events persist
 
 (PENDING)
+
+1 - function to load data from local storage
+---------------------------------------------------
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
-/* Event Listeners  */
-/* JQUERY */
+
 var taskSlots = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 var taskSlotsiD = ['#8am', '#9am', '#10am', '#11am', '#12pm', '#1pm', '#2pm', '#3pm', '#4pm', '#5pm', '#6pm'];
 var taskSlotsNum = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 var currentHour = moment().hours();
+
+
+/*          CLICK TO RENAME TASK                       */
 
 $(document).ready(function(){
 for(i=0; i < taskSlots.length; i++) {
@@ -28,10 +30,13 @@ for(i=0; i < taskSlots.length; i++) {
 }
 });
 
+/*          SETS CORRECT COLOR CODING           */
+
 for(i = 0; i < taskSlotsNum.length; i++){
+
   if (currentHour > taskSlotsNum[i]) {
     $(taskSlotsiD[i]).addClass('past');
-    $(taskSlotsiD[i]).children('form').addClass('past').children('input').addClass('past');
+    $(taskSlotsiD[i]).children('form').addClass('past form-8am').children('input').addClass('past');
     console.log('gray' + taskSlotsNum[i])
   }
   else if (currentHour < taskSlotsNum[i]) {
@@ -49,29 +54,29 @@ for(i = 0; i < taskSlotsNum.length; i++){
 
 
 
-/* MODAL */
+/*                   MODAL                   */
 
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+var btn = document.getElementById("myBtn"); // Get the button that opens the modal
+
+
+var span = document.getElementsByClassName("close"); // Get the <span> element that closes the modal
 console.log(span);
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+
+btn.onclick = function() {                // When the user clicks the button, open the modal 
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+
+span.onclick = function() {     // When the user clicks on <span> (x), close the modal
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+
+window.onclick = function(event) {     // When the user clicks anywhere outside of the modal, close it
   if (event.target == modal) {
   modal.style.display = "none";
   }
